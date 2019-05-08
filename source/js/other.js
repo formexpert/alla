a5743bf1c1975e192f486de5f11a3deb55e176a4
Yaroslav Petukhov <yaros.nw@gmail.com>
2019-05-08 13:14:23 +0500

    slider test

---
+++ source/js/other.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+  speed: 1200,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+
+  // Responsive breakpoints
+  breakpointsInverse: true,
+  breakpoints: {
+    420: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
